12e13f8cfc2ee7a528e03372b9bcedd368a45b29
Mouhcine00007 <mouhcine.elkhadar@gmail.com>
2023-03-17 01:09:54 +0100

    test4 commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main funtion raldadsreturn 0
- * retunr : alyas 0
- */
+ * main - Entry point
+ *Description: 'the program's description'
+ *Return: Always 0 (Success)
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
